ef6a70a913517a3483d8814f97a74ca002af2cee
ksenoZaur <playboyfrombulanikha@gmail.com>
2019-10-19 17:18:15 +0400

    InitialCommit

---
+++ ControllersBasics/Models/Purchase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ControllersBasics.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public int BookId { get; set; }
+        public DateTime DateBuy { get; set; }
+    }
+}

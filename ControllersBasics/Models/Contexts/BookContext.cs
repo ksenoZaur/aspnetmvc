766e19dc72a993dc8d296abde6c7897374b311e7
ksenoZaur <playboyfrombulanikha@gmail.com>
2019-10-20 12:25:08 +0400

    Добавлена минимальная логика покупки книги

---
+++ ControllersBasics/Models/Contexts/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace ControllersBasics.Models.Contexts
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book() { Title = "Война и Мир", Author = "Лев Толстой", Price = 220.0});
+            base.Seed(context);
+        }
+    }
+}
